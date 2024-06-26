fcf258cfbfface4e9c921412fbda4f8a6d682ebc
Alex Karanja <alexnjorge953@gmail.com>
2024-06-27 02:49:04 +0300

    seeding changes fakers

--- server/seed.py
+++ server/seed.py
@@ -5,6 +5,8 @@ from app import app
 from models import db, Pet
 
 with app.app_context():
+    
+    Pet.query.delete()
 
     # Create an empty list
     pets = []
@@ -13,9 +15,41 @@ with app.app_context():
     pets.append(Pet(name = "Fido", species = "Dog"))
     pets.append(Pet(name = "Whiskers", species = "Cat"))
     pets.append(Pet(name = "Hermie", species = "Hamster"))
+    pets.append(Pet(name = "Slither", species = "Snake"))
+    pets.append(Pet(name = "ther", species = "Snake"))
 
     # Insert each Pet in the list into the database table
     db.session.add_all(pets)
 
     # Commit the transaction
     db.session.commit()
+    
+# from random import choice as rc
+# from faker import Faker
+
+# from app import app
+# from models import db, Pet
+
+# with app.app_context():
+
+#     # Create and initialize a faker generator
+#     fake = Faker()
+
+#     # Delete all rows in the "pets" table
+#     Pet.query.delete()
+
+#     # Create an empty list
+#     pets = []
+
+#     species = ['Dog', 'Cat', 'Chicken', 'Hamster', 'Turtle']
+
+#     # Add some Pet instances to the list
+#     for n in range(10):
+#         pet = Pet(name=fake.first_name(), species=rc(species))
+#         pets.append(pet)
+
+#     # Insert each Pet in the list into the "pets" table
+#     db.session.add_all(pets)
+
+#     # Commit the transaction
+#     db.session.commit()    
